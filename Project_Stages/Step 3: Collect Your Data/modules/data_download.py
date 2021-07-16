e200fc27c55d5d71758ad11ca7175eb009c68665
rafael-roano <rafael.roano@gmail.com>
2021-07-15 18:54:33 -0700

    New project folder structure

--- Project_Stages/Step 3: Collect Your Data/modules/data_download.py
+++ Project_Stages/Step 3: Collect Your Data/modules/data_download.py
@@ -3,7 +3,7 @@ import boto3
 star_time = time.time()
 
 '''
-Module to download csv files (transactional data) from s3 bucket and save them in local machine.
+Module to download CSV files (transactional data) from an s3 bucket and save them in local machine.
 '''
 
 # Start timer for script running time
@@ -19,8 +19,10 @@ s3.meta.client.download_file("aaa-raw-data", "customer.csv", "/home/jv/Python/SB
 print("customer.csv successfully downloaded to local machine from aaa-raw-data bucket")
 s3.meta.client.download_file("aaa-raw-data", "qbclass.csv", "/home/jv/Python/SB/SB_Projects/Open-ended_Capstone/data/qbclass.csv")
 print("qbclass.csv successfully downloaded to local machine from aaa-raw-data bucket")
-s3.meta.client.download_file("aaa-raw-data", "customfield.csv", "/home/jv/Python/SB/SB_Projects/Open-ended_Capstone/data/customfield.csv")
-print("customfield.csv successfully downloaded to local machine from aaa-raw-data bucket")
+s3.meta.client.download_file("aaa-raw-data", "part.csv", "/home/jv/Python/SB/SB_Projects/Open-ended_Capstone/data/part.csv")
+print("part.csv successfully downloaded to local machine from aaa-raw-data bucket")
+s3.meta.client.download_file("aaa-raw-data", "product.csv", "/home/jv/Python/SB/SB_Projects/Open-ended_Capstone/data/product.csv")
+print("product.csv successfully downloaded to local machine from aaa-raw-data bucket")
 
 # Calculate and print script running time
 script_time = round(time.time() - star_time, 2)
--- Project_Stages/Step 3: Collect Your Data/modules/data_upload.py
+++ Project_Stages/Step 3: Collect Your Data/modules/data_upload.py
@@ -4,22 +4,23 @@ import pandas as pd
 import time
 
 '''
-Module to extract data from MySQL database (transactional data), save into to csv files and upload them to s3 bucket.
-Scheduled to run once a day in tower server using windows task schduler.
+Module to extract data from MySQL database (transactional data), save it into csv files and upload them to an s3 bucket.
+Scheduled to run once a day in tower server using Windows task scheduler.
 '''
 
 # Start timer for script running time
 star_time = time.time()
 
-# Interface to MySQL database 
-engine = create_engine("mysql+pymysql://" + ":" + "@localhost:3305/" + "3a17")
+# Interface to MySQL database
+engine = create_engine("mysql+pymysql://" + "" + "@localhost:3305/" + "")
 
 # Read tables from database into dataframes
 so = pd.read_sql_table("so", engine)
 soitem = pd.read_sql_table("soitem", engine)
 customer = pd.read_sql_table("customer", engine)
 qbclass = pd.read_sql_table("qbclass", engine)
-customfield = pd.read_sql_table("customfield", engine)
+part = pd.read_sql_table("part", engine)
+product = pd.read_sql_table("product", engine)
 
 # Save dataframes as csv files on tower server
 so.to_csv("C:\\Users\\FBL Server\\Documents\\Python\\SB\\so.csv", index = False)
@@ -30,8 +31,10 @@ customer.to_csv("C:\\Users\\FBL Server\\Documents\\Python\\SB\\customer.csv", in
 print("customer.csv successfully saved on server")
 qbclass.to_csv("C:\\Users\\FBL Server\\Documents\\Python\\SB\\qbclass.csv", index = False)
 print("qbclass.csv successfully saved on server")
-customfield.to_csv("C:\\Users\\FBL Server\\Documents\\Python\\SB\\customfield.csv", index = False)
-print("customfield.csv successfully saved on server")
+part.to_csv("C:\\Users\\FBL Server\\Documents\\Python\\SB\\part.csv", index = False)
+print("part.csv successfully saved on server")
+product.to_csv("C:\\Users\\FBL Server\\Documents\\Python\\SB\\product.csv", index = False)
+print("product.csv successfully saved on server")
 print()
 
 
@@ -46,8 +49,10 @@ s3.meta.client.upload_file("C:\\Users\\FBL Server\\Documents\\Python\\SB\\custom
 print("customer.csv successfully uploaded to aaa-raw-data bucket")
 s3.meta.client.upload_file("C:\\Users\\FBL Server\\Documents\\Python\\SB\\qbclass.csv", "aaa-raw-data", "qbclass.csv")
 print("qbclass.csv successfully uploaded to aaa-raw-data bucket")
-s3.meta.client.upload_file("C:\\Users\\FBL Server\\Documents\\Python\\SB\\customfield.csv", "aaa-raw-data", "customfield.csv")
-print("customfield.csv successfully uploaded to aaa-raw-data bucket")
+s3.meta.client.upload_file("C:\\Users\\FBL Server\\Documents\\Python\\SB\\part.csv", "aaa-raw-data", "part.csv")
+print("part.csv successfully uploaded to aaa-raw-data bucket")
+s3.meta.client.upload_file("C:\\Users\\FBL Server\\Documents\\Python\\SB\\product.csv", "aaa-raw-data", "product.csv")
+print("product.csv successfully uploaded to aaa-raw-data bucket")
 
 
 # Calculate and print script running time
